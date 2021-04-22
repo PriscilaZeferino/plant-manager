4c741a4293e7711b9491e4b5b6e93ed3baedf3ea
Priscila Zeferino <45722071+PriscilaZeferino@users.noreply.github.com>
2021-04-22 16:10:02 -0300

    Confirmation Screen

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import {useFonts, Jost_400Regular, Jost_600SemiBold} from '@expo-google-fonts/jost';
 
-import { UserIdentification } from './src/pages/UserIdentification';
+import { Confirmation } from './src/pages/Confirmation';
 
 export default function App () {
 
@@ -17,7 +17,7 @@ export default function App () {
   }
 
   return (
-    <UserIdentification/>
+    <Confirmation/>
   )
 }
 
